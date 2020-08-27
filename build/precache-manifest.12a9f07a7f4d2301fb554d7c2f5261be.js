05fde7fc19d6f2ab822bb62f183bc23894ea683a
ajir <aji.rusmayran@quadras.co.id>
2020-08-27 19:33:14 +0700

    update

---
+++ build/precache-manifest.12a9f07a7f4d2301fb554d7c2f5261be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d255213e30fef480e084b4881a8738a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02d1da3207eab5d678a0",
+    "url": "/static/css/main.25665c36.chunk.css"
+  },
+  {
+    "revision": "b3bac2ae784b182a8477",
+    "url": "/static/js/2.86e1c6a6.chunk.js"
+  },
+  {
+    "revision": "02d1da3207eab5d678a0",
+    "url": "/static/js/main.cc098335.chunk.js"
+  },
+  {
+    "revision": "3bdd964bc9a7b48912a1",
+    "url": "/static/js/runtime-main.3742ae34.js"
+  },
+  {
+    "revision": "b476f349d321965330257121e9672e93",
+    "url": "/static/media/background.b476f349.jpg"
+  },
+  {
+    "revision": "4a48f8365d1cde9667c9fd395c253da2",
+    "url": "/static/media/kb.4a48f836.png"
+  },
+  {
+    "revision": "2623986b6b56ee0fe35f9b38c183b45b",
+    "url": "/static/media/logo.2623986b.png"
+  }
+]);
